65c1f262d0637335b28a732545209fb763e25426
vagrant <galit.gerasimov3@gmail.com>
2021-03-03 23:10:39 +0000

    Added a display for total inventory and number of categories available in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
+  
   def show
+    @product_num = Product.sum(:quantity)
+    @category_num = Category.count
   end
 end
